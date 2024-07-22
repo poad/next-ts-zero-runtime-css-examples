1f188af0f46294ed57affb4a0a2cf978f18ab185
Kenji Saito <ken-yo@mbr.nifty.com>
2024-07-23 08:53:25 +0900

    Bumps node modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,109 @@
-import nextPlugin from '@next/eslint-plugin-next';
-import reactPlugin from 'eslint-plugin-react';
-import hooksPlugin from 'eslint-plugin-react-hooks';
-import storybookPlugin from 'eslint-plugin-storybook';
-import typescriptParser from '@typescript-eslint/parser';
+// @ts-check
 
-export default [
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import stylisticJsx from '@stylistic/eslint-plugin-jsx';
+import nextPlugin from '@next/eslint-plugin-next';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+// @ts-ignore
+import importPlugin from 'eslint-plugin-import';
+
+import tseslint from 'typescript-eslint';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat();
+
+export default tseslint.config(
   {
-    languageOptions: {
-      parser: typescriptParser,
-    },
-    files: ['**/*.ts', '**/*.tsx'],
     ignores: [
       '**/*.d.ts',
-      '*.js',
+      '*.{js,jsx}',
       'src/tsconfig.json',
-      'src/next-env.d.ts',
       'src/stories',
+      '**/*.css',
       'node_modules/**/*',
+      './.next/*',
+      'out',
+      '.storybook',
     ],
+  },
+  {
+    files: ['src/**/*.{jsx,ts,tsx}'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...compat.config({
+    extends: ['plugin:storybook/recommended'],
+    ignorePatterns: ['!.storybook', 'storybook-static'],
+  }),
+  {
+    files: ['src/**/*.{jsx,tsx}'],
     plugins: {
-      react: reactPlugin,
-      'react-hooks': hooksPlugin,
+      ['jsx-a11y']: jsxA11yPlugin,
+    },
+    extends: [
+      ...compat.config(reactHooksPlugin.configs.recommended),
+      ...compat.config(jsxA11yPlugin.configs.recommended),
+    ],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      formComponents: ['Form'],
+      linkComponents: [
+        { name: 'Link', linkAttribute: 'to' },
+        { name: 'NavLink', linkAttribute: 'to' },
+      ],
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+  },
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    plugins: {
+      import: importPlugin,
+    },
+    extends: [
+      ...tseslint.configs.recommended,
+      ...compat.config(importPlugin.configs.recommended),
+      ...compat.config(importPlugin.configs.typescript),
+    ],
+    settings: {
+      'import/internal-regex': '^~/',
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx'],
+        },
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+  },
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    plugins: {
       '@next/next': nextPlugin,
     },
+    // @ts-ignore
     rules: {
-      ...reactPlugin.configs['jsx-runtime'].rules,
-      ...hooksPlugin.configs.recommended.rules,
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs['core-web-vitals'].rules,
-      ...storybookPlugin.configs.recommended.rules,
       '@next/next/no-duplicate-head': 'off',
       '@next/next/no-img-element': 'error',
       '@next/next/no-page-custom-font': 'off',
     },
   },
   {
-    ignores: ['./.next/*'],
+    rules: {
+      'react/display-name': 'off',
+      'import/namespace': 'off',
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+    },
   },
-];
+);
